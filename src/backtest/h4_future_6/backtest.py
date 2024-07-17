b7cacd8ecc72f369aae243d642e8ff69b9cd1126
Бортников Никита <nikita.bortnikov@gazprombank.ru>
2024-07-17 13:54:29 +0400

    fix path

--- src/backtest/h4_future_6/backtest.py
+++ src/backtest/h4_future_6/backtest.py
@@ -5,7 +5,7 @@ import datetime
 import sys
 import os
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
 from data_preparation import (
     FeaturesConfig, 
     TrainingDataConfig,
